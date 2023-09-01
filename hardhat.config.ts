389e496bf62bb8d1341bd81505e6659c67f2aff5
김보석 <jewel@coredot.io>
2023-09-01 12:54:34 +0900

    hardhat 기본 세팅

---
+++ hardhat.config.ts
@@ -0,0 +1,11 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {},
+  },
+};
+
+export default config;
